cd2ab0c41c5bfb6ea8f7bf223767bc28d93b413e
chrispeng85 <95447953+chrispeng85@users.noreply.github.com>
2025-02-27 17:20:18 +0000

    Update med_image.py

--- med_image.py
+++ med_image.py
@@ -240,6 +240,10 @@ def main():
     train_transforms = transforms.Compose([
 
         transforms.ToPILImage(),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomRotation(10),
+        transforms.RandomAffine(0, shear = 10, scle = (0.8,1.2)),
+        transforms.ColorJitter(brightness = 0.2, contrast = 0.2),
         transforms.ToTensor(),
         transforms.Normalize(mean = [0.485, 0.456, 0.406],
                              std = [0.229, 0.224, 0.225])
