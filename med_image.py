e867717b0140c7212355db85eaa896befae40bf1
chrispeng85 <95447953+chrispeng85@users.noreply.github.com>
2025-02-27 21:18:17 +0000

    Update med_image.py

--- med_image.py
+++ med_image.py
@@ -30,72 +30,104 @@ class ResidualBlock(nn.Module):
             nn.BatchNorm2d(out_channels)
         )
         self.downsample = downsample
+        
+        
+
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
+        
         self.out_channels = out_channels
 
     def forward(self, x):
         residual  = x
         out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
         out = self.conv2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual 
+        out = self.bn2(out) + self.skip(x)
 
         out = self.relu(out)
 
         return out
-    
-
-class Resnet(nn.Module):
-    def __init__(self, block, layers, num_classes = 10):
-
-        super(Resnet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3 , 64, kernel_size = 7, stride = 2, padding = 3),
-            nn.BatchNorm2d(64),
-            nn.ReLU()
-
-        )
-        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride = 1 )
-        self.layer1 = self._make_layer(block ,128, layers[1], stride = 2)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.fc = nn.Linear(512, num_classes)
-    
-    def _make_layer(self, block, planes, blocks, stride = 1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes:
-
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes, kernel_size = 1, stride = stride),
-                nn.BatchNorm2d(planes),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-        return nn.Sequential(*layers)
 
 
+class Dropout(nn.Module):
+
+    def __init__(self,p):
+        super().__init__()
+        self.p = p
+        #dropout probability
+
+    def forward(self, input):
+        p = self.p
+        bernoulli_distr = torch.distributions.bernoulli.Bernoulli(torch.tensor[p])
+        index_zero = bernoulli_distr.sample(input.shape).squeeze()
+        index_zero = index_zero.type(torch.BoolTensor)
+        input[index_zero] = 0
+        return 1/(1-p) * input
+
+class BatchNorm(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+
+        self.gamma = nn.Parameter(torch.ones(num_features))
+        self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, input):
+
+        eps = 1e-5
+        batch_mean = torch.mean(input, 0)
+        temp = torch.zeros_like(input)
+        for i in range(input.shape[1]):
+            temp[:, i, :] = (input[:, i, :] - batch_mean[i, :])**2
+        batch_var = torch.mean(temp, 0)
+
+        for i in range(input.shape[1]):
+            input[:, i, :] = (input[:, i, :] - batch_mean[i, :])/torch.sqrt(batch_var[i, :] + eps)
+            input[:, i, :] = self.gamma[i] * input[:, i, :] + self.beta[i]
+
+        return input
+
+
+class ResidualStack(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, num_blocks):
+        super().__init__()
+        self.residual_stack = nn.ModuleList([ResidualBlock(in_channels, out_channels, stride)])
+        self.residual_stack.extend([ResidualBlock(out_channels, out_channels, 1) for i in range(1, num_blocks)])
+
+    def forward(self, input):
+        out = input
+        for layer in self.residual_stack:
+            out = layer(out)
+
+        return out
+
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = fun
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.maxpool(x)
-        x = self.layer0(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
+        return self.func(x)
 
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+class ResNet(nn.Module):
+    def __init__(self, num_classes, n =2):
+        super(ResNet, self).__init__()
+        self.resnet = nn.Sequential(
 
-        return x  
+            nn.Conv2d(3, 16 ,3, padding = 1, stride = 1, bias = False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            ResidualStack(16,16,1,n),
+            ResisualStack(16,32,2,n),
+            ResidualStack(32,64,2,n),
+            nn.AvgPool2d(8),
+            Lambda(lambda x: x.squeeze)),
+            nn.Linear(64, num_classes)
+        )
+    def forward(self, x):
+        return self.resnet(x)
+
 
 class dataset(Dataset):
 
